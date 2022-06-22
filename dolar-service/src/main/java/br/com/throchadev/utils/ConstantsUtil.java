d96339d462aa89e651feb58bfa1f4dc8deb13432
thiagorocha <thiago.rocha@soulisto.com.br>
2022-06-22 15:22:51 -0300

    add package utlis

---
+++ dolar-service/src/main/java/br/com/throchadev/utils/ConstantsUtil.java
@@ -0,0 +1,11 @@
+package br.com.throchadev.utils;
+
+import lombok.AccessLevel;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
+public class ConstantsUtil {
+
+  public static final String PATH_SEPARATOR_BAR = "/";
+  public static final String PATH_COIN = "USD-BRL";
+}
